127ac31e0006923db2200392b5e5164882d62868
Nightingales21 <nitin.harish21@gmail.com>
2025-04-26 21:04:00 +0530

    Final

--- Hashing/MergeSort.c
+++ Hashing/MergeSort.c
@@ -1,40 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void Split(int A[], int size)
+void Merge(int a[], int l, int m, int r)
 {
-    int l[size/2], r[size/2];
-    int mid = size/2;
-    for (int i = 0; i < mid; i++)
+    int left_length = m - l + 1;
+    int right_length = r-m;
+
+    int templeft[left_length];
+    int tempright[right_length];
+
+    int i,j,k;
+
+    for ( i = 0; i < left_length; i++)
     {
-        l[i] = A[i];
-        printf("%d\t", l[i]);
+        templeft[i] = a[l + i];
     }
-    printf("\n");
-    for (int j = mid; j < size; j++)
+    for ( j = 0; j < right_length; j++)
     {
-        r[j] = A[j];
-        printf("%d\t", r[j]);
+        tempright[j] = a[m+1+j];
+    }
+
+    for ( i = 0, j = 0, k = l; k<=r; k++ )
+    {
+        if ((i < left_length && j >= right_length) || templeft[i] <= tempright[j])
+        {
+            a[k] = templeft[i];
+            i++;
+        }
+        else
+        {
+            a[k] = tempright[j];
+            j++;
+        }
+    }
+}
+
+void MergeSortRecursion(int a[], int l, int r)
+{
+    if (l < r)
+    {
+        int m = (l + (r-l))/2;
+        MergeSortRecursion(a,l,m);
+        MergeSortRecursion(a, m+1, r);
+        Merge(a,l,m,r); 
     }
 
 }
 
-int * MergeSort(int A[])
+void MergeSort(int a[], int length)
 {
-    
+    MergeSortRecursion(a, 0, length - 1);
 }
 
 void main()
 {
-    int size;
-    printf("Enter size of the array: ");
-    scanf("%d", &size);
-    int *A = (int *)malloc(sizeof(int) * size);
-
-    for (int i = 0; i < size; i++)
+    int length = 9;
+    int a[] = {1,9,6,7,8,2,3,4,5};
+    MergeSort(a,length);
+    for (int i = 0; i < length; i++)
     {
-        printf("Enter element %d: ", i+1);
-        scanf("%d", &A[i]);
+        printf("%d\t", a[i]);
     }
-    Split(A, size);
 }
