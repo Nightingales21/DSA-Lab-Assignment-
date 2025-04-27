982eef8f1a7de368f5a03902777448c31bb7c236
Nightingales21 <nitin.harish21@gmail.com>
2025-04-27 11:09:10 +0530

    LinProb Final

---
+++ Hashing/LinearProbing.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int HashFunction(int data)
+{
+    int key;
+    key = data % 100;
+    return key;
+}
+
+void Insert(int Hash_Table[], int dataset[])
+{
+    int key;
+    for (int i=0; i<100; i++)
+    {
+        key = HashFunction(dataset[i]);
+        while (Hash_Table[key] != -1)
+        {
+            key++;
+            if (key == 100)
+            {
+                key = 0;
+            }
+        }
+        Hash_Table[key] = dataset[i];
+    }
+}
+
+void main()
+{
+    int Hash_Table[100];
+    for (int i=0; i<100; i++)
+    {
+        Hash_Table[i] = -1;
+    }
+    srand(time(NULL));
+    int dataset[100];
+    for (int i = 0; i<100; i++)
+    {
+        dataset[i] = rand() % 100 +1;
+    }
+    Insert(Hash_Table, dataset);
+    for (int i=0; i<100; i++)
+    {
+        printf("%d\t", Hash_Table[i]);
+    }
+}
